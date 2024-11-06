28f0b44e0adc7948d7850fe67ca8c654b2aeafb0
Tonmoy <tonmoyhosen767@gmail.com>
2024-11-06 22:20:53 +0600

    categories stacking problem solved

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -4,7 +4,7 @@ import { AllCards } from "./AllCards";
 const Container = () => {
   const [categories, setCategories] = useState([]);
   useEffect(() => {
-    fetch("categories.json")
+    fetch("/categories.json")
       .then((res) => res.json())
       .then((data) => setCategories(data));
   }, []);
